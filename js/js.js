7a94e485ae26deb060b6ce853575eba68ecc63a0
Patricia <patricia@Patricias-MacBook-Air.local>
2021-02-18 17:02:11 +0000

    added chat, email and user part

--- js/js.js
+++ js/js.js
@@ -1,5 +1,23 @@
+"use strict";
 $(document).ready(function () {
+  // *********CHAT**************
+  //removing text from text area on focus
+  $(".chat__textarea").focus(function () {
+    $(".chat__textarea").val("");
+  });
+  //adding text to chat
+  $(".chat__button").click(function () {
+    const text = $(".chat__textarea").val();
+    if (text != "") {
+      $(".chat__content").prepend(
+        "<p  class='chat__paragraph'><strong>Isabel:</strong> " + text + "</p>"
+      );
+    }
+  });
+
+  // *********SPORTS**************
+  // FAQ slide effect
   $(".enquiries__list-item").click(function () {
-    $(this).next().slideToggle(800);
+    $(this).next().slideToggle(500);
   });
 });
