b80d88979147267d83e4b21c02ae77e83b755838
Patricia <patricia@Patricias-MacBook-Air.local>
2021-03-09 12:09:52 +0000

    chat functionality

--- js/js.js
+++ js/js.js
@@ -1,23 +1,64 @@
 "use strict";
-$(document).ready(function () {
-  // *********CHAT**************
-  //removing text from text area on focus
-  $(".chat__textarea").focus(function () {
-    $(".chat__textarea").val("");
-  });
-  //adding text to chat
-  $(".chat__button").click(function () {
-    const text = $(".chat__textarea").val();
-    if (text != "") {
-      $(".chat__content").prepend(
-        "<p  class='chat__paragraph'><strong>Isabel:</strong> " + text + "</p>"
-      );
-    }
+
+// *********CHAT**************
+const num = $(".icons__icon-number").text();
+localStorage.setItem("num", num);
+//change number of chats when chat icon gets clicked
+$(".icons__link--chat").click(function () {
+  const newNum = $(".icons__icon-number").text("0");
+  localStorage.setItem("num", newNum);
+});
+
+//removing text from text area on focus
+$(".chat__textarea").focus(function () {
+  $(".chat__textarea").val("");
+});
+//adding text to chat
+$(".chat__button").click(function () {
+  const text = $(".chat__textarea").val();
+  if (text != "") {
+    $(".chat__content").append(
+      "<p  class='chat__paragraph'><strong>Isabel:</strong> " + text + "</p>"
+    );
+    //adding scroll effect to the chat anytime a new message is entered
+    $(".chat__content")
+      .stop()
+      .animate({
+        scrollTop: $(".chat__content")[0].scrollHeight,
+      });
+  }
+});
+
+//private chat functionality
+
+//list item click
+
+$(".chat__item")
+  .not(":first")
+  .on("click", function () {
+    $(".chat__title").hide();
+    $(".chat__content").find("p").remove();
+    $(".chat__invisible").find("p").remove();
+    $(".chat__textarea").val("Write anything...");
+    $(".chat__invisible").css("display", "flex");
+
+    let userName = $(this).text();
+
+    $(".chat__invisible").prepend(
+      "<p  class='chat__paragraph'>Say something to<strong> " +
+        userName +
+        ":</strong></p>"
+    );
   });
 
-  // *********SPORTS**************
-  // FAQ slide effect
-  $(".enquiries__list-item").click(function () {
-    $(this).next().slideToggle(500);
-  });
+//back to general button
+$(".chat__button-general").click(function () {
+  $(".chat__invisible").css("display", "none");
+  $(".chat__content").find("p").remove();
+  $(".chat__title").show();
+});
+// *********SPORTS**************
+// FAQ slide effect
+$(".enquiries__list-item").click(function () {
+  $(this).next().slideToggle(800);
 });
